8748723110bc5c93f11b54cbade00938b409a928
mWENx <men.wenx@gmail.com>
2023-01-20 10:55:42 -0500

    Added features

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.greetings_button)
+        button.setOnClickListener{
+            Log.v("Hello World", "Button clicked!")
+            Toast.makeText(this, "Nice to meet you ~ ", Toast.LENGTH_LONG).show()
+        }
     }
 }
